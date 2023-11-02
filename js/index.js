368b318d37620c819868fca7f2e9ada7d4dbaa1c
Lucas Regly Marchitto <127771087+Regly-Lucas@users.noreply.github.com>
2023-11-02 20:47:42 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function insert(number) {
+    const numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + number;
+}
+
+function clean() {
+    document.getElementById('resultado').innerHTML = "";
+}
+
+function back() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular() {
+    const resultado = document.getElementById('resultado').innerHTML;
+    if (resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }
+
+}
